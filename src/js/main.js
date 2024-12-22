738b70929a167ae39e2b4cb5baa813208ceaa81e
Flexander-afk <lavriks911@gmail.com>
2024-12-23 01:13:33 +0400

    Revert "LAB4-2 Добавление ранее созданных страниц в проект"

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-window.onload = () => {
-    const savedTheme = localStorage.getItem('theme');
-    const body = document.body;
-
-    if (savedTheme === 'dark') {
-        body.classList.add('dark-mode');
-    }
-    renderTasks(); // Вызов функции для отображения задач при загрузке
-};
